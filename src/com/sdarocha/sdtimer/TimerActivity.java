52aa3070af656c80e07df1dc4d4de9867fe9181d
Sebastien DA ROCHA <sebastien.darocha@actia.fr>
2013-12-29 23:01:36 +0100

    First Interface and Counter implementation

--- src/com/sdarocha/sdtimer/TimerActivity.java
+++ src/com/sdarocha/sdtimer/TimerActivity.java
@@ -1,15 +1,52 @@
 package com.sdarocha.sdtimer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class TimerActivity extends Activity {
+	TextView timer = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timer);
+		
+		timer = (TextView) findViewById(R.id.textViewCounter);
+		
+		final View button1 = findViewById(R.id.button1);
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				new CountDownTimer((1*60+0)*1000, 1000){
+
+					@Override
+					public void onFinish() {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@SuppressLint("DefaultLocale")
+					@Override
+					public void onTick(long millisUntilFinished) {
+						// TODO Auto-generated method stub
+						long hours = 	(millisUntilFinished % (60*60*60*1000)) / (60*60*1000);
+						long minutes = 	(millisUntilFinished % (60*60*1000)) / (60*1000);
+						long secs = 	(millisUntilFinished % (60*1000)) / (1000);
+						String display = String.format("%02d:%02d:%02d", hours, minutes, secs); 
+						timer.setText(display);
+					}
+				
+				}.start();
+
+			}
+		
+		});
 	}
 
 	@Override
